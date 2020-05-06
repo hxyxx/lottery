3d1b80b09ab12a573d3ad72b20eb533ed4496ba3
terry <xutianhao@mmac.local>
2020-05-06 22:09:10 +0800

    update dao

--- models/lt_blackip.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type LtBlackip struct {
-	Id         int    `xorm:"not null pk autoincr comment('主键') INT(11)"`
-	Ip         string `xorm:"comment('IP地址') VARCHAR(50)"`
-	Blacktime  int    `xorm:"comment('黑名单限制到期时间') INT(11)"`
-	SysCreated int    `xorm:"comment('创建时间') INT(11)"`
-	SysUpdated int    `xorm:"comment('修改时间') INT(11)"`
-}
