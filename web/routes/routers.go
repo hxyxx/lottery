e30c0ec140186b3fbe4291cb3b6cf0642224ea32
terry <xutianhao@mmac.local>
2020-05-28 10:28:27 +0800

    update user management

--- web/routes/routers.go
+++ web/routes/routers.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/kataras/iris/_examples/mvc/login/web/middleware"
 	"github.com/kataras/iris/mvc"
 	"hxyxx/lottery/bootstrap"
 	"hxyxx/lottery/services"
@@ -16,4 +17,28 @@ func Configure(b *bootstrap.Bootstrapper){
 	index := mvc.New(b.Party("/"))
 	index.Register(userService,giftService,codeService,resultService,blackipService)
 	index.Handle(new(controller.IndexController))
+
+
+	admin := mvc.New(b.Party("/admin"))
+	admin.Router.Use(middleware.BasicAuth)
+	admin.Register(userService,giftService,codeService,resultService,blackipService)
+	admin.Handle(new(controller.AdminController))
+
+	adminGift :=admin.Party("/gift")
+	adminGift.Register(giftService)
+	adminGift.Handle(new(controller.AdminGiftController))
+
+	adminCode := admin.Party("/code")
+	adminCode.Register(codeService)
+	adminCode.Handle(new(controller.AdminCodeController))
+
+	adminResult := admin.Party("/result")
+	adminResult.Register(resultService)
+	adminResult.Handle(new(controller.AdminResultController))
+
+	adminBlackip := admin.Party("/user")
+	adminBlackip.Register(userService)
+	adminBlackip.Handle(new(controller.AdminUserController))
+
+
 }
